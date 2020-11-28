70e41b3282a9bfbd78864f4a9fc099b6f748b073
Adam Jacyno <jacynoadam@gmail.com>
2020-11-28 02:36:00 +0100

    Initialize repo
    Some basics starting scripts as well as some useful free assets created / imported

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody2D rb2;
+
+    void Start () {
+
+    }
+
+    void Update () {
+
+    }
+
+    void FixedUpdate () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        rb2.MovePosition (new Vector2 (mousePos.x, gameObject.transform.position.y));
+    }
+
+    void OnCollisionEnter2D (Collision2D c) {
+        Debug.Log ("COLLISION!");
+    }
+}
